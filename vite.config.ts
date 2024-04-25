c854b9241e713bb4d774479e9c1900b03bf6ec7c
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-04-25 17:26:11 +0100

    first working version

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import {defineConfig} from 'vite';
+import { defineConfig } from "vite";
 // import {resolve} from 'node:path';
 import * as path from "node:path";
 // @ts-ignore
@@ -9,46 +9,43 @@ import * as packageJson from "./package.json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const getPackageName = () => {
-    return packageJson.name;
+  return packageJson.name;
 };
 
 const getPackageNameCamelCase = () => {
-    try {
-        return getPackageName().replace(/-./g, char => char[1].toUpperCase());
-    } catch (err) {
-        throw new Error("Name property in package.json is missing.");
-    }
+  try {
+    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
 };
 
 const fileName = {
-    es: `${getPackageName()}.mjs`,
-    cjs: `${getPackageName()}.cjs`,
-    iife: `${getPackageName()}.iife.js`,
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+  iife: `${getPackageName()}.iife.js`,
 };
 
 const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
-    build: {
-        outDir: "./build/dist",
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: getPackageNameCamelCase(),
-            formats,
-            fileName: format => fileName[format],
-        }
+  base: "./",
+  build: {
+    outDir: "./build/dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: getPackageNameCamelCase(),
+      formats,
+      fileName: (format) => fileName[format],
     },
-    test: {},
-    // resolve: {
-    //     alias: [
-    //         { find: "@", replacement: path.resolve(__dirname, "src") },
-    //         { find: "@@", replacement: path.resolve(__dirname) },
-    //     ],
-    // },
-    plugins: [
-        tsconfigPaths()
-    ]
+  },
+  test: {},
+  // resolve: {
+  //     alias: [
+  //         { find: "@", replacement: path.resolve(__dirname, "src") },
+  //         { find: "@@", replacement: path.resolve(__dirname) },
+  //     ],
+  // },
+  plugins: [tsconfigPaths()],
 });
